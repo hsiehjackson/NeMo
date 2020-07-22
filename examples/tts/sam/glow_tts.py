30c07e5f5a32444da62fa15292178d7d4f7a5bc4
Samuel Kriman <samuelkriman@gmail.com>
2020-07-21 22:49:09 -0500

    save dir

--- examples/tts/sam/glow_tts.py
+++ examples/tts/sam/glow_tts.py
@@ -17,7 +17,7 @@ def main(hps):
     model = nemo_tts_sam.models.glow_tts.GlowTTSModel(hps)
     tb_logger = pl_loggers.TensorBoardLogger(hps.train.model_dir)
     lr_logger = LearningRateLogger()
-    checkpoint_callback = ModelCheckpoint()
+    checkpoint_callback = ModelCheckpoint(hps.train.model_dir)
     trainer = Trainer.from_argparse_args(
         hps.train, gpus=-1, distributed_backend='dp', logger=tb_logger, callbacks=[lr_logger, checkpoint_callback]
     )
