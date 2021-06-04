f59490dbc96b2ab5184ef3c594253d102eb837c4
Samuel Kriman <samuelkriman@gmail.com>
2021-06-03 22:03:15 -0500

    lego asr
    Signed-off-by: Samuel Kriman <samuelkriman@gmail.com>

---
+++ nemo/collections/asr/modules/lego_encoder.py
@@ -0,0 +1,209 @@
+#encoder link conformer but accepts list of Lego layer configs
+
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+
+from nemo.collections.asr.parts.submodules.conformer_modules import ConformerLayer
+from nemo.collections.asr.parts.submodules.lego_modules import LegoBlock
+from nemo.collections.asr.parts.submodules.multi_head_attention import PositionalEncoding, RelPositionalEncoding
+from nemo.collections.asr.parts.submodules.subsampling import ConvSubsampling
+from nemo.core.classes.common import typecheck
+from nemo.core.classes.exportable import Exportable
+from nemo.core.classes.module import NeuralModule
+from nemo.core.neural_types import AcousticEncodedRepresentation, LengthsType, NeuralType, SpectrogramType
+
+__all__ = ['LegoEncoder']
+
+
+class LegoEncoder(NeuralModule, Exportable):
+    """
+    Lego Encoder
+
+    Args:
+        sub_blocks (ListConfig): list of configs for the sub-blocks within each block
+        feat_in (int): the size of feature channels
+        n_blocks (int): number of blocks of ConformerBlock
+        d_model (int): the hidden size of the model
+        feat_out (int): the size of the output features
+            Defaults to -1 (means feat_out is d_model)
+        subsampling (str): the method of subsampling, choices=['vggnet', 'striding']
+            Defaults to striding.
+        subsampling_factor (int): the subsampling factor which should be power of 2
+            Defaults to 4.
+        subsampling_conv_channels (int): the size of the convolutions in the subsampling module
+            Defaults to -1 which would set it to d_model.
+        dropout (float): the dropout rate used in all sub-blocks.
+            Defaults to 0.1.
+        pos_emb_mode (str): type of positional embedding
+            'rel_pos': relative positional embedding and Transformer-XL
+            'abs_pos': absolute positional embedding and Transformer
+            default is rel_pos.
+        pos_emb_max_len (int): the maximum length of positional embeddings
+            Defaulst to 5000
+    """
+
+    def input_example(self):
+        """
+        Generates input examples for tracing etc.
+        Returns:
+            A tuple of input examples.
+        """
+        input_example = torch.randn(16, self._feat_in, 256).to(next(self.parameters()).device)
+        input_example_length = torch.randint(0, 256, (16,)).to(next(self.parameters()).device)
+        return tuple([input_example, input_example_length])
+
+    @property
+    def input_types(self):
+        """Returns definitions of module input ports.
+        """
+        return OrderedDict(
+            {
+                "audio_signal": NeuralType(('B', 'D', 'T'), SpectrogramType()),
+                "length": NeuralType(tuple('B'), LengthsType()),
+            }
+        )
+
+    @property
+    def output_types(self):
+        """Returns definitions of module output ports.
+        """
+        return OrderedDict(
+            {
+                "outputs": NeuralType(('B', 'D', 'T'), AcousticEncodedRepresentation()),
+                "encoded_lengths": NeuralType(tuple('B'), LengthsType()),
+            }
+        )
+
+    def __init__(
+        self,
+        sub_blocks,
+        feat_in,
+        n_blocks,
+        d_model,
+        feat_out=-1,
+        subsampling='striding',
+        subsampling_factor=4,
+        subsampling_conv_channels=-1,
+        dropout=0.1,
+        pos_emb_mode='abs_pos',
+        pos_emb_max_len=5000,
+    ):
+        super().__init__()
+
+        self.d_model = d_model
+        self._feat_in = feat_in
+        self.scale = math.sqrt(self.d_model)
+
+        if subsampling_conv_channels == -1:
+            subsampling_conv_channels = d_model
+        if subsampling and subsampling_factor > 1:
+            self.pre_encode = ConvSubsampling(
+                subsampling=subsampling,
+                subsampling_factor=subsampling_factor,
+                feat_in=feat_in,
+                feat_out=d_model,
+                conv_channels=subsampling_conv_channels,
+                activation=nn.ReLU(),
+            )
+            self._feat_out = d_model
+        else:
+            self._feat_out = d_model
+            self.pre_encode = nn.Linear(feat_in, d_model)
+
+        #should only leave rel pos embs?
+        pos_bias_u = None
+        pos_bias_v = None
+
+        if pos_emb_mode == "rel_pos":
+            self.pos_enc = RelPositionalEncoding(
+                d_model=d_model,
+                dropout_rate=dropout,
+                max_len=pos_emb_max_len,
+                xscale=self.xscale,
+                dropout_rate_emb=dropout,
+            )
+        elif pos_emb_mode == "abs_pos":
+            pos_bias_u = None
+            pos_bias_v = None
+            self.pos_enc = PositionalEncoding(
+                d_model=d_model, dropout_rate=dropout, max_len=pos_emb_max_len, xscale=self.xscale
+            )
+        else:
+            raise ValueError(f"Not valid positional embedding mode: '{pos_emb_mode}'!")
+
+        self.blocks = nn.ModuleList()
+        for i in range(n_blocks):
+            block = LegoBlock(sub_blocks)
+            self.blocks.append(block)
+
+        if feat_out > 0 and feat_out != self.output_dim:
+            self.out_proj = nn.Linear(self.feat_out, feat_out)
+            self._feat_out = feat_out
+        else:
+            self.out_proj = None
+            self._feat_out = d_model
+
+    @typecheck()
+    def forward(self, audio_signal, length=None):
+        if length is None:
+            length = torch.tensor(audio_signal.size(-1)).repeat(audio_signal.size(0)).to(audio_signal)
+        audio_signal = torch.transpose(audio_signal, 1, 2)
+
+        if isinstance(self.pre_encode, ConvSubsampling):
+            audio_signal, length = self.pre_encode(audio_signal, length)
+        else:
+            audio_signal = self.pre_encode(audio_signal)
+
+        audio_signal, pos_emb = self.pos_enc(audio_signal)
+        bs, xmax, idim = audio_signal.size()
+
+        # Create the self-attention and padding masks
+        pad_mask = self.make_pad_mask(length, max_time=xmax, device=audio_signal.device)
+        att_mask = pad_mask.unsqueeze(1).repeat([1, xmax, 1])
+        att_mask = att_mask & att_mask.transpose(1, 2)
+        if self.att_context_size[0] >= 0:
+            att_mask = att_mask.triu(diagonal=-self.att_context_size[0])
+        if self.att_context_size[1] >= 0:
+            att_mask = att_mask.tril(diagonal=self.att_context_size[1])
+        att_mask = ~att_mask
+        pad_mask = ~pad_mask
+
+        for lth, layer in enumerate(self.layers):
+            audio_signal = layer(x=audio_signal)
+
+        if self.out_proj is not None:
+            audio_signal = self.out_proj(audio_signal)
+
+        audio_signal = torch.transpose(audio_signal, 1, 2)
+        return audio_signal, length
+
+    @staticmethod
+    def make_pad_mask(seq_lens, max_time, device=None):
+        """Make masking for padding."""
+        bs = seq_lens.size(0)
+        seq_range = torch.arange(0, max_time, dtype=torch.int32)
+        seq_range_expand = seq_range.unsqueeze(0).expand(bs, max_time)
+        seq_lens = seq_lens.type(seq_range_expand.dtype).to(seq_range_expand.device)
+        seq_length_expand = seq_lens.unsqueeze(-1)
+        mask = seq_range_expand < seq_length_expand
+
+        if device:
+            mask = mask.to(device)
+        return mask
---
+++ nemo/collections/asr/parts/submodules/lego_modules.py
@@ -0,0 +1,176 @@
+# add conv, fourier, attn, linear layers
+
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import torch
+from torch import nn as nn
+from torch.nn import LayerNorm
+
+from nemo.collections.asr.parts.submodules.multi_head_attention import (
+    MultiHeadAttention,
+    RelPositionMultiHeadAttention,
+)
+from nemo.collections.asr.parts.utils.activations import Swish
+from nemo.core.classes.module import NeuralModule
+
+# LegoBlock is initialized based on list of sub_block configs
+
+__all__ = ['LegoBlock', 'LegoConvSubBlock', 'LegoFourierSubBlock', 'LegoLinearSubBlock']
+
+
+# Lego block and all the sub-block classes here
+
+
+class LegoBlock(NeuralModule):
+    """A single Lego block. Is initialized using a list config of Lego sub-blocks.
+
+    Args:
+        sub_blocks (ListConfig): Sub-blocks.
+        d_model (int): input number of channels/model dimension
+        (maybe)
+        dropout (float): Dropout rate for each sub-block.
+        norm (str): Normalization mode for each sub-block.
+        outer_residual (bool): Add residual connection from start to end of block.
+    """
+
+    def __init__(
+            self,
+            sub_blocks,
+            d_model,
+            dropout=0.1,
+            outer_residual=False
+    ):
+        super(LegoBlock, self).__init__()
+
+        self.sub_blocks = nn.ModuleList()
+        self.norms = nn.ModuleList()
+
+        for sub_cfg in sub_blocks:
+            self.norms.append(LayerNorm(d_model))
+            self.sub_blocks.append(LegoBlock.from_config_dict(sub_cfg))
+
+            #if we want rel pos encoding, we should check here if attn is present
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.final_norm = LayerNorm(d_model)
+
+        self.outer_residual = outer_residual
+
+    def forward(self, x, att_mask=None, pos_emb=None, pad_mask=None):
+        """
+        Args:
+            x (torch.Tensor): input signals (B, T, d_model)
+            att_mask (torch.Tensor): attention masks(B, T, T)
+            pos_emb (torch.Tensor): (L, 1, d_model)
+            pad_mask (torch.tensor): padding mask
+        Returns:
+            x (torch.Tensor): (B, T, d_model)
+        """
+
+        if self.outer_residual:
+            outer_residual = x
+
+        for norm, sub_block in zip(self.norms, self.sub_blocks):
+            residual = x
+            x = norm(x)
+            x = sub_block(x)
+            x = self.dropout(x)
+            x = residual + x
+
+        x = self.final_norm(x)
+
+        if self.outer_residual:
+            x = outer_residual + x
+
+        return x
+
+
+class LegoConvSubBlock(nn.Module):
+
+    def __init__(self, d_model, kernel_size=17, axis="time"):
+        super(LegoConvSubBlock, self).__init__()
+        assert (kernel_size - 1) % 2 == 0
+        self.d_model = d_model
+
+        self.depthwise_conv = nn.Conv1d(
+            in_channels=d_model,
+            out_channels=d_model,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=(kernel_size - 1) // 2,
+            groups=d_model,
+            bias=True,
+        )
+
+        self.activation = nn.GELU()
+
+        self.axis = axis
+
+    def forward(self, x, pad_mask=None):
+        if self.axis == "time":
+            x = x.transpose(-2, -1)
+            if pad_mask is not None:
+                x.masked_fill_(pad_mask.unsqueeze(1), 0.0)
+
+        x = self.depthwise_conv(x)
+        x = self.activation(x)
+
+        if self.axis == "time":
+            x = x.transpose(-2, -1)
+
+        return x
+
+
+class LegoFourierSubBlock(nn.Module):
+    # more stuff here later
+
+    def __init__(self, dim=-1, norm="ortho", use_fft2=False):
+        super(LegoFourierSubBlock, self).__init__()
+
+        self.dim = dim
+        self.norm = norm
+        self.use_fft2 = use_fft2
+
+
+    def forward(self, x, pad_mask=None):
+        if self.use_fft2:
+            x = torch.fft.fft2(x, dim=self.dim, norm=self.norm)
+        else:
+            x = torch.fft.fft(x, dim=self.dim, norm=self.norm)
+
+        return x.real
+
+
+class LegoLinearSubBlock(nn.Module):
+
+    def __init__(self, d_model, shared_weight_groups=1):
+        super(LegoLinearSubBlock, self).__init__()
+
+        assert d_model % shared_weight_groups == 0
+
+        self.group_size = d_model // shared_weight_groups
+
+        self.linear = nn.Linear(self.group_size, self.group_size)
+
+    def forward(self, x):
+        original_shape = x.shape
+        x = x.view(x.shape[:-1] + (-1, self.group_size))
+
+        x = self.linear(x)
+
+        x = x.view(original_shape)
+
+        return x
