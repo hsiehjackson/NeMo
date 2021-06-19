e6785663799c45fac002264e50a11c79a84d6d6e
Samuel Kriman <samuelkriman@gmail.com>
2021-06-19 15:25:51 -0500

    upd
    Signed-off-by: Samuel Kriman <samuelkriman@gmail.com>

--- nemo/collections/asr/modules/lego_encoder.py
+++ nemo/collections/asr/modules/lego_encoder.py
@@ -114,7 +114,7 @@ class LegoEncoder(NeuralModule, Exportable):
         self._feat_in = feat_in
         self.scale = math.sqrt(self.d_model)
 
-        """if subsampling_conv_channels == -1:
+        if subsampling_conv_channels == -1:
             subsampling_conv_channels = d_model
         if subsampling and subsampling_factor > 1:
             self.pre_encode = ConvSubsampling(
@@ -125,9 +125,9 @@ class LegoEncoder(NeuralModule, Exportable):
                 conv_channels=subsampling_conv_channels,
                 activation=nn.ReLU(),
             )
-            self._feat_out = d_model"""
+            self._feat_out = d_model
         #else:"""
-        self.pre_encode = nn.Sequential(nn.Linear(feat_in, d_model), nn.ReLU())
+        #self.pre_encode = nn.Sequential(nn.Linear(feat_in, d_model), nn.ReLU())
 
         pos_bias_u = None
         pos_bias_v = None
