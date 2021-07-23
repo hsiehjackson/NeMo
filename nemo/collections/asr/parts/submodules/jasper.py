81ae5aa9a9194488d530c7569398d6e478eaccfc
Samuel Kriman <samuelkriman@gmail.com>
2021-07-23 17:55:03 -0500

    fix
    Signed-off-by: Samuel Kriman <samuelkriman@gmail.com>

--- nemo/collections/asr/parts/submodules/jasper.py
+++ nemo/collections/asr/parts/submodules/jasper.py
@@ -91,9 +91,8 @@ def dct1(x):
     :return: the DCT-I of the signal over the last dimension
     """
     x_shape = x.shape
-    x = x.view(-1, x_shape[-1])
 
-    return torch.fft.rfft(torch.cat([x, x.flip([1])[..., 1:-1]], dim=-1)).real.view(*x_shape)
+    return torch.fft.rfft(torch.cat([x, x.flip([-1])[..., 1:-1]], dim=-1)).real.view(*x_shape)
 
 
 def create_dct_matrix(n):
