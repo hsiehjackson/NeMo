17907995ab6750dd899a8e254abfc61226b201cc
Samuel Kriman <samuelkriman@gmail.com>
2021-08-04 10:31:09 -0500

    mask fix
    Signed-off-by: Samuel Kriman <samuelkriman@gmail.com>

--- nemo/collections/asr/models/ctc_models.py
+++ nemo/collections/asr/models/ctc_models.py
@@ -552,7 +552,7 @@ class EncDecCTCModel(ASRModel, ExportableEncDecModel, ASRModuleMixin):
         masked_spectrograms = processed_signal.detach()
         spec_masks = torch.logical_and(masked_spectrograms < 1e-5, masked_spectrograms > -1e-5).float()
         for idx, proc_len in enumerate(processed_signal_length):
-            spec_masks[:, :, proc_len:] = 0.
+            spec_masks[idx, :, proc_len:] = 0.
 
 
         encoded, encoded_len = self.encoder(audio_signal=processed_signal, length=processed_signal_length)
