b9294a136703cd49ecfc2aa77a05e9f69cd99e13
sam1373 <samuelkriman@gmail.com>
2022-01-15 19:54:29 -0800

    import
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/core/classes/mixins/access_mixins.py
+++ nemo/core/classes/mixins/access_mixins.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from omegaconf import DictConfig
 from abc import ABC
 from contextlib import contextmanager
 from enum import Enum
