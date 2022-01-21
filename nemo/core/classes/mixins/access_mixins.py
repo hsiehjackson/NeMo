d9fd4a4ec7b45b03f36a65142ed5116808b3ec0d
sam1373 <samuelkriman@gmail.com>
2022-01-21 14:29:15 -0800

    access
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/core/classes/mixins/access_mixins.py
+++ nemo/core/classes/mixins/access_mixins.py
@@ -82,7 +82,8 @@ class AccessMixin(ABC):
         """
         Recursively reset the registry of distillation tensors to clear up memory.
         """
-        self._registry.clear()
+        del self._registry
+        self._registry = []
         for _, m in self.named_modules():
             if hasattr(m, '_registry') and len(m._registry) > 0:
                 m.reset_registry()
