98ab0cc08ceb1fd9b5017dd670082d46b1fc5534
sam1373 <samuelkriman@gmail.com>
2022-01-21 11:55:08 -0800

    fix
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/collections/asr/parts/submodules/conformer_modules.py
+++ nemo/collections/asr/parts/submodules/conformer_modules.py
@@ -125,7 +125,7 @@ class ConformerLayer(torch.nn.Module, AccessMixin):
         x = self.norm_out(x)
 
         if self.access_cfg.get('access_all_intermediate', False):
-            self.register_accessible_tensor(tensor=out)
+            self.register_accessible_tensor(tensor=x)
 
         return x
 
