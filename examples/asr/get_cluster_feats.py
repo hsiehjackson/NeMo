bfc567e4fa283f580561645c3fcab68b4a9cc5fe
sam1373 <samuelkriman@gmail.com>
2022-01-29 06:43:34 -0800

    combine 4, feat script
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- examples/asr/get_cluster_feats.py
+++ examples/asr/get_cluster_feats.py
@@ -78,23 +78,27 @@ class FeatClusteringConfig:
     # Required configs
     model_path: Optional[str] = None  # Path to a .nemo file
     pretrained_name: Optional[str] = None  # Name of a pretrained model
-    audio_dir: Optional[str] = None  # Path to a directory which contains audio files
     dataset_manifest: Optional[str] = None  # Path to dataset's JSON manifest
+
+    cluster_model_path: Optional[str] = None # if already have cluster model
 
     # General configs
     output_filename: Optional[str] = None
     batch_size: int = 32
+    sample_rate: int = 16000
 
     # Set `cuda` to int to define CUDA device. If 'None', will look for CUDA
     # device anyway, and do inference on CPU only if CUDA device is not found.
     # If `cuda` is a negative number, inference will be on CPU only.
     cuda: Optional[int] = None
     amp: bool = False
-    audio_type: str = "wav"
+    #audio_type: str = "wav"
 
     #clustering params
-    feats_for_fit: int = 50000
+    num_feats_for_fit: int = 50000
     n_clusters: int = 100
+
+    layer_name: str = "7"
 
     access: AccessConfig = AccessConfig()
 
@@ -132,16 +136,9 @@ def main(cfg: TranscriptionConfig) -> TranscriptionConfig:
         asr_model = ASRModel.from_pretrained(model_name=cfg.pretrained_name, map_location=device)  # type: ASRModel
         model_name = cfg.pretrained_name
 
-    print(asr_model._registry)
-
-    trainer = pl.Trainer(gpus=[cfg.cuda] if cfg.cuda >= 0 else 0)
-    asr_model.set_trainer(trainer)
     asr_model = asr_model.eval()
 
-    # Setup decoding strategy
-    if hasattr(asr_model, 'change_decoding_strategy'):
-        asr_model.change_decoding_strategy(cfg.rnnt_decoding)
-
+    """
     # get audio filenames
     if cfg.audio_dir is not None:
         filepaths = list(glob.glob(os.path.join(cfg.audio_dir, f"*.{cfg.audio_type}")))
@@ -153,6 +150,7 @@ def main(cfg: TranscriptionConfig) -> TranscriptionConfig:
                 item = json.loads(line)
                 filepaths.append(item['audio_filepath'])
     logging.info(f"\nTranscribing {len(filepaths)} files...\n")
+    """
 
     # setup AMP (optional)
     if cfg.amp and torch.cuda.is_available() and hasattr(torch.cuda, 'amp') and hasattr(torch.cuda.amp, 'autocast'):
@@ -164,110 +162,131 @@ def main(cfg: TranscriptionConfig) -> TranscriptionConfig:
         def autocast():
             yield
 
-    # Compute output filename
-    if cfg.output_filename is None:
-        # create default output filename
-        if cfg.audio_dir is not None:
-            cfg.output_filename = os.path.dirname(os.path.join(cfg.audio_dir, '.')) + '.json'
-        else:
-            cfg.output_filename = cfg.dataset_manifest.replace('.json', f'_{model_name}.json')
 
-    # if transcripts should not be overwritten, and already exists, skip re-transcription step and return
-    if not cfg.overwrite_transcripts and os.path.exists(cfg.output_filename):
-        logging.info(
-            f"Previous transcripts found at {cfg.output_filename}, and flag `overwrite_transcripts`"
-            f"is {cfg.overwrite_transcripts}. Returning without re-transcribing text."
-        )
-
-        return cfg
-
-    #TODO
     #create dataloader like in transcribe
-    ds_cfg = model_cfg.train_ds
-    ds_cfg.manifest_filepath = cfg.dataset_manifest
-    asr_model.setup_training_data(train_data_config=ds_cfg)
-    datalayer = asr_model._train_dl
+    #ds_cfg = model_cfg.train_ds
+    #ds_cfg.manifest_filepath = cfg.dataset_manifest
+    #asr_model.setup_training_data(train_data_config=ds_cfg)
+    #datalayer = asr_model._train_dl
+
+    ds_cfg = {
+        'manifest_filepath': cfg.dataset_manifest,
+        'sample_rate': cfg.sample_rate,
+        'batch_size': cfg.batch_size,
+        'trim_silence': False,
+        'shuffle': False,
+        'num_workers': 16,
+        'pin_memory': True,
+    }
+
+    ds_cfg = OmegaConf.create(ds_cfg)
+
+    ds_cfg.return_sample_id = True
+    ds_cfg.min_duration = None
+    ds_cfg.max_duration = None
+    ds_cfg.shuffle = False
+
+    datalayer = asr_model._setup_dataloader_from_config(ds_cfg)
 
     #process batches
 
     asr_model.eval()
+    asr_model.to(device)
     feats_for_fit = []
-    for batch in tqdm(datalayer, desc="Obtaining features for fit"):
+    total_feats = 0
 
-        feats = asr_model.get_feats(
-            input_signal=batch[0].to(device), input_signal_length=batch[1].to(device)
+    #print(datalayer)
+    print(len(datalayer.dataset))
+
+    #indexes = set()
+
+    for batch in tqdm(datalayer, desc="Obtaining features for fit"):
+        #for i in batch[-1]:
+        #    indexes.add(int(i))
+
+        input_signal = batch[0].to(device)
+        input_signal_length = batch[1].to(device)
+
+        feats, feat_lens = asr_model.get_feats(
+            input_signal=input_signal,
+            input_signal_length=input_signal_length,
+            layer_name=cluster_cfg.layer_name
         )
 
-        all_feats.append(feats)
+        feats = feats.reshape(-1, feats.shape[-1])
+        total_feats += feats.shape[0]
+
+        feats_for_fit.append(feats)
 
         del batch
 
-        if len(feats_for_fit) * ds_cfg.batch_size > cfg.feats_for_fit:
+        if total_feats > cfg.num_feats_for_fit:
             break
 
-    print(len(feats_for_fit))
-    print(feats_for_fit[0].shape)
     feats_for_fit = torch.cat(feats_for_fit, dim=0).cpu()
 
     #do clustering
 
     cluster_model = MiniBatchKMeans(n_clusters=cfg.n_clusters,
-                                    max_iter=100,
+                                    max_iter=10000,
                                     tol=0.0,
-                                    max_no_improvement=100,
+                                    max_no_improvement=200,
                                     n_init=20,
-                                    reassignment_ratio=0.0,
-                                    batch_size=10000)
-
+                                    reassignment_ratio=0.01,
+                                    batch_size=10000,
+                                    verbose=True)
 
     cluster_model.fit(feats_for_fit)
     inertia = -cluster_model.score(feats_for_fit) / len(feats_for_fit)
-    logger.info("total intertia: %.5f", inertia)
-    logger.info("finished successfully")
+    print("total intertia: %.5f", inertia)
+    print("finished successfully")
+
+
+    #produce labels
+
+    label_dict = {}
+    dur_dict = {}
 
     cluster_labels = []
     for batch in tqdm(datalayer, desc="Getting cluster labels"):
-        feats = asr_model.get_feats(
-            input_signal=batch[0].to(device), input_signal_length=batch[1].to(device)
+
+        input_signal = batch[0].to(device)
+        input_signal_length = batch[1].to(device)
+
+        feats, feat_lens = asr_model.get_feats(
+            input_signal=input_signal,
+            input_signal_length=input_signal_length,
+            layer_name=cluster_cfg.layer_name
         )
 
+        orig_bs = feats.shape[0]
+
+        feats = feats.reshape(-1, feats.shape[-1])
         cur_labels = cluster_model.predict(feats.cpu())
+        cur_labels = cur_labels.reshape(orig_bs, -1)
+
+        for j in range(cur_labels.shape[0]):
+            label_dict[int(batch[-1][j])] = cur_labels[j, :feat_lens[j]]
+            dur_dict[int(batch[-1][j])] = int(input_signal_length[j])
 
         del batch
 
 
 
-
-
-
-
     ###################
-    # transcribe audio
-    with autocast():
-        with torch.no_grad():
-            transcriptions = asr_model.transcribe(filepaths, batch_size=cfg.batch_size)
-    logging.info(f"Finished transcribing {len(filepaths)} files !")
 
-    logging.info(f"Writing transcriptions into file: {cfg.output_filename}")
-
-    # if transcriptions form a tuple (from RNNT), extract just "best" hypothesis
-    if type(transcriptions) == tuple and len(transcriptions) == 2:
-        transcriptions = transcriptions[0]
+    logging.info(f"Writing labels into file: {cfg.output_filename}")
 
     # write audio transcriptions
     with open(cfg.output_filename, 'w', encoding='utf-8') as f:
-        if cfg.audio_dir is not None:
-            for idx, text in enumerate(transcriptions):
-                item = {'audio_filepath': filepaths[idx], 'pred_text': text}
-                f.write(json.dumps(item) + "\n")
-        else:
-            with open(cfg.dataset_manifest, 'r') as fr:
+        with open(cfg.dataset_manifest, 'r') as fr:
                 for idx, line in enumerate(fr):
                     item = json.loads(line)
-                    item['pred_text'] = transcriptions[idx]
+                    item['labels'] = list(map(int, label_dict[idx]))
                     f.write(json.dumps(item) + "\n")
 
-    logging.info("Finished writing predictions !")
+    logging.info("Finished writing labels !")
+
     return cfg
 
 
