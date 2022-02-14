7798d0c16dcc2be63169fd7e6459e7b2cd508774
sam1373 <samuelkriman@gmail.com>
2022-02-13 17:07:57 -0800

    update
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/collections/asr/modules/audio_preprocessing.py
+++ nemo/collections/asr/modules/audio_preprocessing.py
@@ -673,7 +673,8 @@ class MaskedPatchAugmentation(NeuralModule):
 
             tr = augmented_spec.transpose(-2, -1)
             time_steps = tr.shape[-2]
-            print(tr[idx].reshape(time_steps // 4, -1).mean(-1))
+            cur_len //= 4
+            print(tr[idx].reshape(time_steps // 4, -1).mean(-1)[:cur_len])
 
         if self.spec_augment is not None:
             augmented_spec = self.spec_augment(input_spec=augmented_spec, length=length)
