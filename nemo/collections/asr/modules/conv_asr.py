fe0fe486cd03b2288c0405f42e20e8da62731d46
sam1373 <samuelkriman@gmail.com>
2022-02-16 16:04:29 -0800

    add blank by default
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/collections/asr/modules/conv_asr.py
+++ nemo/collections/asr/modules/conv_asr.py
@@ -417,7 +417,7 @@ class ConvASRDecoder(NeuralModule, Exportable):
     def output_types(self):
         return OrderedDict({"logprobs": NeuralType(('B', 'T', 'D'), LogprobsType())})
 
-    def __init__(self, feat_in, num_classes, init_mode="xavier_uniform", vocabulary=None, add_blank=False):
+    def __init__(self, feat_in, num_classes, init_mode="xavier_uniform", vocabulary=None, add_blank=True):
         super().__init__()
 
         if vocabulary is None and num_classes < 0:
