22c92b152548b378c3a2debde956adc9954ca4b4
sam1373 <samuelkriman@gmail.com>
2023-02-28 15:20:54 -0800

    up
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- examples/asr/test_max_dur.py
+++ examples/asr/test_max_dur.py
@@ -11,6 +11,7 @@ import torch
 from nemo.collections.asr.models import ASRModel
 from nemo.utils import logging, model_utils
 
+import time
 
 @dataclass
 class EvaluationConfig():
@@ -38,7 +39,7 @@ def main(cfg: EvaluationConfig):
         # Binary search
         min_mins = 0
         max_mins = 1200
-        while max_mins - min_mins > 2:
+        while max_mins - min_mins > 1:
             test_minutes = (max_mins + min_mins) // 2
             model_cfg = ASRModel.restore_from(restore_path=cfg.model_dir + "/" + model_name, return_config=True)
             classpath = model_cfg.target  # original class path
@@ -58,18 +59,22 @@ def main(cfg: EvaluationConfig):
 
             try:
                 with torch.no_grad():
+                    start_time = time.time()
                     asr_model.forward(input_signal=input_signal_long, input_signal_length=length_long)
-                print(model_name, "passed", test_minutes, "minutes")
+                    end_time = time.time()
+                elapsed_time = end_time - start_time  # calculate the elapsed time
+                #print(f"The function took {elapsed_time:.6f} seconds to run.")
+                print(model_name, "passed", test_minutes, "minutes, taking", {elapsed_time:.6f}, "seconds")
                 min_mins = test_minutes
             except RuntimeError as e:
-                print(e)
+                #print(e)
                 print(model_name, "ran out of memory on", test_minutes, "minutes")
                 for p in asr_model.parameters():
                     if p.grad is not None:
                         del p.grad  # free some memory
                 torch.cuda.empty_cache()
                 max_mins = test_minutes
-        print('maximum minutes for ' + model_name + ' is ' + str(max_mins))
+        print('---maximum minutes for ' + model_name + ' is ' + str(max_mins))
 
 
 if __name__ == '__main__':
