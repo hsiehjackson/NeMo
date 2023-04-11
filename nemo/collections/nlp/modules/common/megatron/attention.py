f2a4157319ad809a50622678141027b72d8755df
sam1373 <samuelkriman@gmail.com>
2023-04-10 20:44:04 -0700

    repeat
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/attention.py
+++ nemo/collections/nlp/modules/common/megatron/attention.py
@@ -979,7 +979,13 @@ class CoreAttention(MegatronModule):
                     ).transpose(1, 2)
 
                     # global_key_attn = torch.softmax(global_key_attn, dim=-1).masked_fill(attention_mask, 0.0)
-                    global_key_attn = self.scale_mask_softmax(global_key_attn, attention_mask)
+                    rep_mask = repeat(
+                        attention_mask,
+                        'b h t m -> b (h h2) t (m m2)',
+                        h2=global_key_attn.shape[1],
+                        m2=global_key_attn.shape[3],
+                    )
+                    global_key_attn = self.scale_mask_softmax(global_key_attn, rep_mask)
                     if not self.sequence_parallel:
                         with tensor_parallel.random.get_cuda_rng_tracker().fork():
                             global_key_attn = self.attention_dropout(global_key_attn)
@@ -1273,7 +1279,14 @@ class CoreAttention(MegatronModule):
 
         # compute global attn probs
         # global_attn_probs = nn.functional.softmax(global_attn_scores, dim=-1)
-        global_attn_probs = self.scale_mask_softmax(global_attn_scores, is_index_masked.transpose(2, 3))
+        is_index_masked = is_index_masked.transpose(2, 3)
+        rep_is_index_masked = repeat(
+            is_index_masked,
+            'b h m t -> b (h h2) (m m2) t',
+            h2=global_attn_scores.shape[1],
+            m2=global_attn_scores.shape[2],
+        )
+        global_attn_probs = self.scale_mask_softmax(global_attn_scores, rep_is_index_masked)
 
         global_attn_probs = global_attn_probs.view(batch_size * h, max_num_global_attn_indices, seq_len)
 
