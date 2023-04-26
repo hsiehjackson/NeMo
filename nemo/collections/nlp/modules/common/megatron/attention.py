ac74da25d4770bba35180f9711a5acc008bb0169
sam1373 <samuelkriman@gmail.com>
2023-04-26 15:58:19 -0700

    norm fix
    Signed-off-by: sam1373 <samuelkriman@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/attention.py
+++ nemo/collections/nlp/modules/common/megatron/attention.py
@@ -159,6 +159,30 @@ def _get_local_attention_mask(attention_mask: torch.Tensor, block_len: int, devi
     return local_attention_mask.unsqueeze(1).to(device)
 
 
+class LongT5LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        Construct a layernorm module in the LongT5 style. No bias and no subtraction of mean.
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        # LongT5 uses a layer_norm which only scales and doesn't shift, which is also known as Root Mean
+        # Square Layer Normalization https://arxiv.org/abs/1910.07467 thus varience is calculated
+        # w/o mean and there is no bias. Additionally we want to make sure that the accumulation for
+        # half-precision inputs is done in fp32
+
+        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+
+        # convert into half-precision if necessary
+        if self.weight.dtype in [torch.float16, torch.bfloat16]:
+            hidden_states = hidden_states.to(self.weight.dtype)
+
+        return self.weight * hidden_states
+
 
 class ParallelAttention(MegatronModule, adapter_mixins.AdapterModuleMixin):
     """Parallel self-attention layer abstract class.
@@ -279,7 +303,7 @@ class ParallelAttention(MegatronModule, adapter_mixins.AdapterModuleMixin):
             )
 
         if self.transient_global_tokens:
-            self.transient_norm = torch.nn.LayerNorm(hidden_size)
+            self.transient_norm = LongT5LayerNorm(hidden_size)
 
         self.core_attention = CoreAttention(
             layer_number=self.layer_number,
